e4e2765713fa3b6064e57bd22fc621f1cbe92d63
cofffeen <91495423+cofffeen@users.noreply.github.com>
2024-11-13 19:02:25 +0300

    Добавлены тесты

--- square.py
+++ square.py
@@ -1,7 +1,31 @@
+import unittest
 
 def area(a):
+    # принимает число a, возвращает площадь квадрата со стороной a
     return a * a
 
 
 def perimeter(a):
+    # принимает число a, возвращает периметр квадрата со стороной a
     return 4 * a
+
+class RectangleTestCase(unittest.TestCase):
+    def test_zero_area(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_area(self):
+        res = area(2)
+        self.assertEqual(res, 4)
+
+    def test_zero_perimeter(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+
+
+    def test_perimeter(self):
+        res = perimeter(2)
+        self.assertEqual(res, 8)
+
+if __name__ == '__main__':
+    unittest.main()
